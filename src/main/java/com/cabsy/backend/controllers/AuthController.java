6cd616b25d4404262fc2a89cb6daaae1ba6ae97f
Priyanka <2408341@cognizant.com>
2025-06-06 18:21:03 +0530

    complete merge

--- src/main/java/com/cabsy/backend/controllers/AuthController.java
+++ src/main/java/com/cabsy/backend/controllers/AuthController.java
@@ -16,28 +16,14 @@ import com.cabsy.backend.dtos.DriverResponseDTO;
 import com.cabsy.backend.dtos.LoginDTO;
 import com.cabsy.backend.dtos.UserRegistrationDTO;
 import com.cabsy.backend.dtos.UserResponseDTO;
-import com.cabsy.backend.models.User;
 import com.cabsy.backend.services.DriverService;
 import com.cabsy.backend.services.UserService; // For DTO validation
 
 import jakarta.validation.Valid; // Needed for login check
-import com.cabsy.backend.services.UserService;
-
 import java.util.Optional;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.validation.Valid; // For DTO validation
-
-import org.springframework.security.crypto.password.PasswordEncoder; // Needed for login check
  
 @RestController
 @RequestMapping("/api/auth")
